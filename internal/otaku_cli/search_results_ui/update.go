be57fe45b9a0e745e74b8996f5fe8967a53970a1
Yusuf Çınar Özmen <marulumbenaq@gmail.com>
2022-11-07 01:45:30 +0300

    Moved init logic to Init() function

--- internal/otaku_cli/search_results_ui/update.go
+++ internal/otaku_cli/search_results_ui/update.go
@@ -4,30 +4,14 @@ import (
 	"github.com/YusufOzmen01/otaku-cli/constants"
 	"github.com/YusufOzmen01/otaku-cli/internal/otaku_cli/details_ui"
 	"github.com/charmbracelet/bubbles/key"
-	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 )
 
 func (m UI) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	if !m.init {
 		m.init = true
 
-		items := make([]list.Item, 0)
-
-		for _, result := range m.Results {
-			items = append(items, list.Item(result))
-		}
-
-		m.list = list.New(items, constants.AnimeResultDelegate{}, 0, 20)
-		m.list.Title = "Anime Search Result"
-		m.list.SetShowStatusBar(true)
-		m.list.SetFilteringEnabled(true)
-		m.list.Styles.Title = lipgloss.NewStyle().MarginLeft(0)
-		m.list.Styles.PaginationStyle = list.DefaultStyles().PaginationStyle.PaddingLeft(2)
-		m.list.Styles.HelpStyle = list.DefaultStyles().HelpStyle.PaddingLeft(2).PaddingBottom(0)
-
-		return m, nil
+		return m, m.Init()
 	}
 
 	switch msg := msg.(type) {
